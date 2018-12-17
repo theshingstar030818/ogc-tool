a8d115eb67510565018a05401590e45eca70d8c6
irtaza91 <irtaza.qayyum@gmail.com>
2018-12-18 02:16:12 +0500

    add project template #17

--- src/app/@theme/components/create-project/create-project.component.ts
+++ src/app/@theme/components/create-project/create-project.component.ts
@@ -11,12 +11,19 @@ export class CreateProjectComponent implements OnInit {
 
   @Input() title: string;
 
+  template: any;
+
   constructor(
     protected ref: NbDialogRef<CreateProjectComponent>,
     protected projectsService: ProjectsService,
   ) { }
 
   ngOnInit() {
+    this.projectsService.getTemplates().then((results) => {
+      this.template = results;
+    }, (error) => {
+      // console.log(error);
+    });
   }
 
   dismiss() {
