5d2f217572cbed8f988b0f9972b1bcd4c0552020
Tanzeel Rehman <tanzeel.rehman@solarwinds.com>
2018-12-27 14:40:25 -0500

    fix lint

--- src/app/tool/project/preview/preview.component.ts
+++ src/app/tool/project/preview/preview.component.ts
@@ -13,79 +13,79 @@ export class PreviewComponent implements OnInit {
     {
       name: 'OGC President: Fares Elsabbagh',
       phone: '613-225-9991 Ext: 202',
-      email: 'fares@ottawageneralcontractors.com'
+      email: 'fares@ottawageneralcontractors.com',
     },
     {
-      name:'General Manager: Bryan Sim',
-      phone:'613-225-9991 Ext: 213',
-      email:'bryan@ottawageneralcontractors.com',
+      name: 'General Manager: Bryan Sim',
+      phone: '613-225-9991 Ext: 213',
+      email: 'bryan@ottawageneralcontractors.com',
     },
     {
-      name:'Project Coordinator: Nick Karrandjas',
-      phone:'613-225-9991 Ext: 103,  or Cell: 613-806-3172',
-      email:'nick@ottawageneralcontractors.com',
+      name: 'Project Coordinator: Nick Karrandjas',
+      phone: '613-225-9991 Ext: 103,  or Cell: 613-806-3172',
+      email: 'nick@ottawageneralcontractors.com',
     },
     {
-      name:'Project Coordinator: Karley Harrington',
-      phone:'613-225-9991 Ext: 825',
-      email:'karley@ottawageneralcontractors.com',
+      name: 'Project Coordinator: Karley Harrington',
+      phone: '613-225-9991 Ext: 825',
+      email: 'karley@ottawageneralcontractors.com',
     },
     {
-      name:'Project Coordinator: Ken Beaubien',
-      phone:'613-225-9991 Ext: 104',
-      email:'ken.b@ottawageneralcontractors.com',
+      name: 'Project Coordinator: Ken Beaubien',
+      phone: '613-225-9991 Ext: 104',
+      email: 'ken.b@ottawageneralcontractors.com',
     },
     {
-      name:'Estimator: Khaled Hamouda',
-      phone:'613-225-9991 Ext: 206',
-      email:'khaled@ottawageneralcontractors.com',
+      name: 'Estimator: Khaled Hamouda',
+      phone: '613-225-9991 Ext: 206',
+      email: 'khaled@ottawageneralcontractors.com',
     },
     {
-      name:'Project Manager: Andrew Jabbour',
-      phone:'613-225-9991 Ext: 108',
-      email:'andrew@ottawageneralcontractors.com',
+      name: 'Project Manager: Andrew Jabbour',
+      phone: '613-225-9991 Ext: 108',
+      email: 'andrew@ottawageneralcontractors.com',
     },
     {
-      name:'Project Manager: Bradley Newsham',
-      phone:'613-225-9991 Ext: 805',
-      email:'bradley@ottawageneralcontractors.com',
+      name: 'Project Manager: Bradley Newsham',
+      phone: '613-225-9991 Ext: 805',
+      email: 'bradley@ottawageneralcontractors.com',
     },
     {
-      name:'Project Manager: Allan Orasuk',
-      phone:'613-225-9991 Ext: 106',
-      email:'allan@ottawageneralcontractors.com',
+      name: 'Project Manager: Allan Orasuk',
+      phone: '613-225-9991 Ext: 106',
+      email: 'allan@ottawageneralcontractors.com',
     },
     {
-      name:'Project Manager: Natalia Pierce',
-      phone:'613-225-9991 Ext: 815',
-      email:'Natalia@ottawageneralcontractors.com',
+      name: 'Project Manager: Natalia Pierce',
+      phone: '613-225-9991 Ext: 815',
+      email: 'Natalia@ottawageneralcontractors.com',
     },
     {
-      name:'Project Manager: Paolo Marinelli',
-      phone:'613-225-9991 Ext: 811',
-      email:'paolo@ottawageneralcontractors.com',
+      name: 'Project Manager: Paolo Marinelli',
+      phone: '613-225-9991 Ext: 811',
+      email: 'paolo@ottawageneralcontractors.com',
     },
     {
-      name:'Design Manager: James Walker',
-      phone:'613-225-9991 Ext: 109',
-      email:'james.walker@ottawageneralcontractors.com',
+      name: 'Design Manager: James Walker',
+      phone: '613-225-9991 Ext: 109',
+      email: 'james.walker@ottawageneralcontractors.com',
     },
     {
-      name:'Designer: Matt Griffiths',
-      phone:'613-225-9991 Ext: 102',
-      email:'matt@ottawageneralcontractors.com',
+      name: 'Designer: Matt Griffiths',
+      phone: '613-225-9991 Ext: 102',
+      email: 'matt@ottawageneralcontractors.com',
     },
     {
-      name:'Material Selection: Tanveen Kaur',
-      phone:'613-225-9991 Ext: 307',
-      email:'tanveen@ottawageneralcontractors.com',
+      name: 'Material Selection: Tanveen Kaur',
+      phone: '613-225-9991 Ext: 307',
+      email: 'tanveen@ottawageneralcontractors.com',
     },
     {
-      name:'Material Selection: Deirdre Crick',
-      phone:'613-225-9991 Ext: 112',
-      email:'deirdre@ottawageneralcontractors.com'
-    }
-  ]
+      name: 'Material Selection: Deirdre Crick',
+      phone: '613-225-9991 Ext: 112',
+      email: 'deirdre@ottawageneralcontractors.com',
+    },
+  ];
 
   divisions = [
     {
@@ -102,13 +102,13 @@ export class PreviewComponent implements OnInit {
               title: '',
               ogcNotes: '',
               tax: 1.13,
-              total: 1
-            }
-          ]
-        }
-      ]
-    }
-  ]
+              total: 1,
+            },
+          ],
+        },
+      ],
+    },
+  ];
 
   constructor(
     private route: ActivatedRoute,
