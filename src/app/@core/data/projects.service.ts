100a7dfffebc720bf8c252695ca04e4c301ce963
Irtaza Qayyum <irtaza.qayyum@gmail.com>
2019-01-03 23:34:49 +0500

    Projects parse #14 (#30)
    * Get Project Data #14
    
    * Remove Project #14
    
    * save project #14
    
    * add project relations #14

--- src/app/@core/data/projects.service.ts
+++ src/app/@core/data/projects.service.ts
@@ -24,19 +24,89 @@ export class ProjectsService {
 
   constructor() {
     this.observableProjects = new BehaviorSubject<any[]>(this.projects);
-    this.addProject(this.mockProject);
+    this.getProjectsParse();
+  }
+
+  async getProjectsParse() {
+
+    const Project = Parse.Object.extend('Project');
+    const query = new Parse.Query(Project);
+    query.include('current');
+    query.include('current.client');
+    const results = await query.find();
+
+    this.projects = results;
+    this.observableProjects.next(this.projects);
   }
 
   //  TODO
-  addProject(project?) {
-    this.projects.push(this.mockProject);
-    this.observableProjects.next(this.projects);
+  async addProject(project?) {
+
+    const ProjectHistory = Parse.Object.extend('ProjectHistory');
+    const projectHistoryObject = new ProjectHistory();
+
+    const Client = Parse.Object.extend('Client');
+    let ClientObject = new Parse.Query(Client);
+    let client = await ClientObject.get(project.client);
+
+    projectHistoryObject.set('name', project.name);
+    projectHistoryObject.set('address', project.address);
+    projectHistoryObject.set('dueDate', project.duedate);
+    projectHistoryObject.set('status', project.status);
+    projectHistoryObject.set('client', client);
+    projectHistoryObject.setACL(new Parse.ACL(Parse.User.current()));
+
+    projectHistoryObject.save().then((result) => {
+
+      let relation = result.relation('templates');
+      for (let template of project.template) {
+
+        relation.add(template);
+      }
+      result.save().then(() => {
+        const Project = Parse.Object.extend('Project');
+        const projectObject = new Project();
+
+        projectObject.set('current', result);
+        projectObject.setACL(new Parse.ACL(Parse.User.current()));
+
+        projectObject.save().then((newProject) => {
+
+          let relationHistory = newProject.relation('history');
+          relationHistory.add(result);
+          newProject.save().then(() => {
+
+            this.projects.push(newProject);
+            this.observableProjects.next(this.projects);
+          });
+
+        }, (error) => {
+           // console.log('Failed to create new object, with error code: ' + error.message);
+        });
+      });
+
+    }, (error) => {
+      // console.log('Failed to create new object, with error code: ' + error.message);
+    });
+
   }
 
   // TODO
   removeProject(event) {
-    this.projects.splice(event.index, 1);
-    this.observableProjects.next(this.projects);
+    let r = confirm('Are You Sure You Want to Delete This Project?');
+    if (r === true) {
+      const Project = Parse.Object.extend('Project');
+      const ProjectObject = new Project();
+      ProjectObject.id = event.data.id;
+      ProjectObject.destroy().then((results) => {
+        // The object was deleted from the Parse Cloud.
+        this.projects.splice(event.index, 1);
+        this.observableProjects.next(this.projects);
+
+      }, (error) => {
+        // console.log(error);
+      });
+    }
   }
 
   getProjects() {
