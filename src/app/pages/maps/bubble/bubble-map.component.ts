474afc0c008dce52fb70ff747ee7af4368762022
Tanzeel Rehman <tanzeel.rehman@solarwinds.com>
2019-04-01 23:53:28 +0500

    update to angular 7

--- src/app/pages/maps/bubble/bubble-map.component.ts
+++ src/app/pages/maps/bubble/bubble-map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
 import { combineLatest } from 'rxjs';
 import { takeWhile } from 'rxjs/operators';
 // import { NgxEchartsService } from 'ngx-echarts';
